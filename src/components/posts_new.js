4e3425f51bccde668c7fa6f625bc584b4f5a7b47
Loris <loris4526@gmail.com>
2017-05-31 13:31:47 +0200

    Display form for creating new posts

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,9 +3,25 @@ import React, {Component} from "react";
 export default class PostsNew extends Component {
   render(){
     return(
-      <h1>
-        New post form here!
-      </h1>
+      <div>
+
+        <form className="pt-5">
+          <div className="form-group">
+            <label>Title</label>
+            <input type="email" className="form-control"aria-describedby="emailHelp" />
+          </div>
+          <div className="form-group">
+            <label>Categories</label>
+            <input type="password" className="form-control"/>
+          </div>
+          <div className="form-group">
+            <label>Categories</label>
+            <textarea className="form-control" rows="3"></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">Save</button>
+          <button type="reset" className="ml-4 btn btn-primary">Cancel</button>
+        </form>
+      </div>
     );
   }
 }
