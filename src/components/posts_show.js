9556646582a0e667f0e275a1a06af823d36f4f6d
Loris <loris4526@gmail.com>
2017-06-01 13:25:15 +0200

    Pass props from reducer to PostsShow component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,9 +1,28 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
+import {fetchPost} from "../actions/index";
 
-const PostsShow = (props)=>(
-  <div>
-    <h1>PostsShow component!!</h1>
-  </div>
-);
+class PostsShow extends Component {
+  componentWillMount(){
+    const {fetchPost,match} = this.props;
+    this.props.fetchPost(match.params.id);
+  }
+  render(){
+    const id = this.props.match.params.id;
+    let {title,categories,content} = this.props.post[id] || {};
+    return(
+      <div>
+        <h1>{title}</h1>
+        
+      </div>
+    )
+  }
+}
 
-export default PostsShow;
+function mapStateToProps(state){
+  return {
+    post: state.posts
+  }
+}
+
+export default connect(mapStateToProps,{fetchPost})(PostsShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,13 +1,15 @@
 //import actions
-import {FETCH_POSTS,CREATE_POST} from "../actions/index";
+import {FETCH_POSTS,FETCH_POST} from "../actions/index";
 import _ from "lodash";
 
 export default function postsReducer(state={}, action){
   switch (action.type) {
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data,"id");
-    case CREATE_POST:
-      return state;
+    case FETCH_POST:
+      return {
+        ...state, [action.payload.data.id]: action.payload.data
+      };
     default:
       return state;
   }
